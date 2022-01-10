d42d30e5d63c931291d12b07c089826521299f05
Michael Brown <mikeb@360replays.com>
2022-01-10 15:37:42 -0800

    Initial commit

---
+++ Binner.StorageProvider.MySql/Binner.StorageProvider.MySql/MySqlStorageConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Binner.StorageProvider.MySql
+{
+    public class MySqlStorageConfiguration
+    {
+        public string ConnectionString { get; set; }
+
+        public MySqlStorageConfiguration()
+        {
+        }
+
+        public MySqlStorageConfiguration(IDictionary<string, string> config)
+        {
+            if (config.ContainsKey("ConnectionString"))
+                ConnectionString = config["ConnectionString"];
+        }
+    }
+}
